835e150731bfff9152d75b8d1aa76b24116776e6
Victor Ay <viktoray007@gmail.com>
2019-09-09 19:32:51 +0300

    add mock template

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-from Flask import Flask
+from flask import Flask, render_template
+from sqlalchemy.orm import sessionmaker
+
+from db_setup import Base, Books
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///bookmanager.db')
+Base.metadata.bind = engine
+db_session = sessionmaker(bind=engine)()
+
+app = Flask('__main__')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ db_setup.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Books(Base):
+    __tablename__ = 'book'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(250), nullable=False)
+    author = Column(String(250), nullable=False)
+    isbn = Column(String(250), nullable=False)
+    comments = Column(String)
+
+engine = create_engine('sqlite:///bookmanager.db')
+
+Base.metadata.create_all(engine)
